43047836cedb2d61ee7cc22ec00fb82b8c2f654c
Kenneth Ashley <ken.u.diggitt@gmail.com>
2019-11-29 16:55:14 -0500

    Add VTHOBurned chart

--- src/components/Chart/VTHOBurned.tsx
+++ src/components/Chart/VTHOBurned.tsx
@@ -1,33 +1,45 @@
-import React, { useEffect, useRef } from 'react';
-import { Chart } from "frappe-charts/dist/frappe-charts.min.esm";
+import React from 'react';
 import { Card } from 'antd';
 import styled from 'styled-components';
+import { ResponsiveLine } from '@nivo/line';
+
+const Wrapper = styled.div`
+  height: 200px;
+`;
 
 function VTHOBurned({ chart: data }: any) {
-  const chartRef = useRef(null);
-
-  useEffect(() => {
-    const chart = new Chart(chartRef.current, {
-      lineOptions: {
-        regionFill: 1
-      },
-      isNavigable: true,
-      colors: ['#d3d3d3'],
-      type: 'line',
-      height: 200,
-      data,
-    });
-
-    return () => {
-      if (chart) {
-        chart.destroy();
-      }
-    }
-  });
-
   return (
     <Card title="VTHO Burned">
-      <div ref={chartRef} />
+      <Wrapper>
+        <ResponsiveLine
+          data={[
+            data
+          ]}
+          margin={{
+            top: 10,
+            right: 10,
+            bottom: 10,
+            left: 10
+          }}
+          yScale={{
+            type: 'linear',
+            stacked: true,
+          }}
+          axisTop={null}
+          axisRight={null}
+          axisLeft={null}
+          enableArea={true}
+          colors={[ '#1890ff' ]}
+          enableGridX={false}
+          pointSize={1}
+          pointColor={{ theme: 'background' }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: 'serieColor' }}
+          axisBottom={null}
+          enableSlices={'x'}
+          useMesh={true}
+        />
+      </Wrapper>
     </Card>
   );
 }
